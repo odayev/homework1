4c19064fa0e6f44cd6678b1f160edc031310839c
Neto test <mr.agash.tm@gmail.com>
2024-03-11 02:10:18 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -33,22 +33,22 @@ const ShopItemFunc = (props) => {
   );
 };*/
 
-export class ShopItemClass extends Component {
+class ShopItemClass extends Component {
   render() {
     return (
       <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div className="description">{item.descriptionFull}</div>
+        <h2>{this.props.item.brand}</h2>
+        <h1>{this.props.item.title}</h1>
+        <h3>{this.props.item.description}</h3>
+        <div className="description">{this.props.item.descriptionFull}</div>
         <div className="highlight-window mobile">
           <div className="highlight-overlay"></div>
         </div>
         <div className="divider"></div>
         <div className="purchase-info">
           <div className="price">
-            {item.currency}
-            {item.price.toFixed(2)}
+            {this.props.item.currency}
+            {this.props.item.price.toFixed(2)}
           </div>
           <button>Добавить в корзину</button>
         </div>
